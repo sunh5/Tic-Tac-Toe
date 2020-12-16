2e86e2b398e240e3edcb2ec3f06bed0f6dbf4898
sunh5 <sunh5@rose-hulman.edu>
2020-12-16 03:19:28 -0500

    update

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameStateLabel: UILabel!
+    @IBOutlet weak var gameStatenavBar: UINavigationBar!
+    
+    @IBOutlet var gameBoardButtons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        updateView()
     }
 
-
+    @IBAction func pressedNewGame(_ sender: Any) {
+        print("New Game")
+        
+        updateView()
+    }
+    
+    @IBAction func pressedGameBoardButton(_ sender: Any) {
+        let button = sender as! UIButton
+        print(button.tag)
+        updateView()
+    }
+    
+    func updateView() {
+        
+    }
 }
 
