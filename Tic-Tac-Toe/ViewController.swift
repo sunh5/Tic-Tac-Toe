ef12eb320fef90c4a01d0c07d126f7b439d6cb0d
sunh5 <sunh5@rose-hulman.edu>
2020-12-16 02:29:47 -0500

    Initial Commit

---
+++ Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Haoxuan Sun on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
